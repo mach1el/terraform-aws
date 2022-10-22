e6d7aaa02e0e2729c6f37f82449bb19e3bc4ad08
mach1el <th3phantom.m1@gmail.com>
2022-10-22 16:05:22 +0700

    Fix index data

--- clone-ec2/modules/ec2/main.tf
+++ clone-ec2/modules/ec2/main.tf
@@ -7,8 +7,8 @@ resource "aws_instance" "new" {
   key_name               = "${data.aws_instance.source_instance[each.value.name].key_name}"
   
   root_block_device {
-    volume_size = "${data.aws_instance.source_instance[each.value.name]['ebs_block_device'].volume_size}"
-    volume_type = "${data.aws_instance.source_instance[each.value.name]['ebs_block_device'].volume_type}"
+    volume_size = "${data.aws_instance.source_instance[each.value.name].ebs_block_device.volume_size}"
+    volume_type = "${data.aws_instance.source_instance[each.value.name].ebs_block_device.volume_type}"
     tags   = {
       Name = "${data.aws_instance.source_instance[each.value.name].tags.Name}-clone"
     }
