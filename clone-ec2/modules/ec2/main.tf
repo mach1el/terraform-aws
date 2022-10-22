9a83df0adc3f923d03b8ba77f1d9f2c41ba7b637
mach1el <th3phantom.m1@gmail.com>
2022-10-22 16:24:45 +0700

    remove ec2 block device

--- clone-ec2/modules/ec2/main.tf
+++ clone-ec2/modules/ec2/main.tf
@@ -6,13 +6,13 @@ resource "aws_instance" "new" {
   vpc_security_group_ids = "${data.aws_instance.source_instance[each.value.name].vpc_security_group_ids}"
   key_name               = "${data.aws_instance.source_instance[each.value.name].key_name}"
   
-  root_block_device {
-    volume_size = "${lookup(data.aws_instance.source_instance[each.value.name].ebs_block_device,volume_size)}"
-    volume_type = "${data.aws_instance.source_instance[each.value.name].ebs_block_device.volume_type}"
-    tags   = {
-      Name = "${data.aws_instance.source_instance[each.value.name].tags.Name}-clone"
-    }
-  }
+  # root_block_device {
+  #   volume_size = "${data.aws_instance.source_instance[each.value.name].ebs_block_device.volume_size}"
+  #   volume_type = "${data.aws_instance.source_instance[each.value.name].ebs_block_device.volume_type}"
+  #   tags   = {
+  #     Name = "${data.aws_instance.source_instance[each.value.name].tags.Name}-clone"
+  #   }
+  # }
 
   tags   = {
     Name = "${data.aws_instance.source_instance[each.value.name].tags.Name}-clone"
