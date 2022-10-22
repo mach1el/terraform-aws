cef3ca435c7429a6fda9beef503fc2122383e670
mach1el <th3phantom.m1@gmail.com>
2022-10-22 14:27:01 +0700

    Fix output

--- clone-ec2/modules/ec2/locals.tf
+++ clone-ec2/modules/ec2/locals.tf
@@ -5,7 +5,7 @@ locals {
 locals {
   cloned = [
     for cloned_ec2 in var.AMIsList: {
-      source_id = cloned_ec2.source_id
+      # source_id = cloned_ec2.source_id
       id        = cloned_ec2.id
       name      = cloned_ec2.tags.Name
     }
--- clone-ec2/modules/ec2/main.tf
+++ clone-ec2/modules/ec2/main.tf
@@ -1,20 +1,20 @@
 resource "aws_instance" "new" {
   for_each               = { for ami in local.AMIs: ami.id => ami }
   ami                    = "${each.value.id}"
-  instance_type          = "${data.aws_instance.source_instance[each.value.source_id].instance_type}"
-  subnet_id              = "${data.aws_instance.source_instance[each.value.source_id].subnet_id}"
-  vpc_security_group_ids = "${data.aws_instance.source_instance[each.value.source_id].vpc_security_group_ids}"
-  key_name               = "${data.aws_instance.source_instance[each.value.source_id].key_name}"
+  instance_type          = "${data.aws_instance.source_instance[each.value.name].instance_type}"
+  subnet_id              = "${data.aws_instance.source_instance[each.value.name].subnet_id}"
+  vpc_security_group_ids = "${data.aws_instance.source_instance[each.value.name].vpc_security_group_ids}"
+  key_name               = "${data.aws_instance.source_instance[each.value.name].key_name}"
   
   root_block_device {
-    volume_size = "${data.aws_instance.source_instance[each.value.source_id].volume_size}"
-    volume_type = "${data.aws_instance.source_instance[each.value.source_id].volume_type}"
+    volume_size = "${data.aws_instance.source_instance[each.value.name].volume_size}"
+    volume_type = "${data.aws_instance.source_instance[each.value.name].volume_type}"
     tags   = {
-      Name = "${data.aws_instance.source_instance[each.value.source_id].tags.Name}-clone"
+      Name = "${data.aws_instance.source_instance[each.value.name].tags.Name}-clone"
     }
   }
 
   tags   = {
-    Name = "${data.aws_instance.source_instance[each.value.source_id].tags.Name}-clone"
+    Name = "${data.aws_instance.source_instance[each.value.name].tags.Name}-clone"
   }
 }
