489f35ab2574c67b2f3baa56c2ad1c3ae92a52e9
mach1el <th3phantom.m1@gmail.com>
2022-11-18 10:03:45 +0700

    Dynamic count

--- ec2/locals.tf
+++ ec2/locals.tf
@@ -1,9 +1,9 @@
 locals {
   serverconfig = [
     for srv in var.configuration : [
-      for i in range(1) : {
+      for i in range(srv.count) : {
         ami             = srv.ami
-        instance_name   = srv.service_name
+        instance_name   = "${srv.service_name}-${sv.count}"
         instance_type   = srv.instance_type
         subnet_id       = srv.subnet_id
         ssh_key_name    = srv.ssh_key_name
