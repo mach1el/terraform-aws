233aeff96e887729718d52e3fb5874ad20d58713
mach1el <th3phantom.m1@gmail.com>
2022-11-19 11:13:18 +0700

    anything in map

--- route53/modules/records/variables.tf
+++ route53/modules/records/variables.tf
@@ -11,11 +11,11 @@ variable private_zone_id {
 }
 
 variable public_records {
-  type    = list(map())
+  type    = list(map(any))
   default = []
 }
 
 variable private_records {
-  type    = list(map())
+  type    = list(map(any))
   default = []
 }
