6837c8530fcee0907acc0988f8c62909d9694b69
mach1el <th3phantom.m1@gmail.com>
2022-11-19 10:58:56 +0700

    Add conditional for dns records

--- route53/modules/records/main.tf
+++ route53/modules/records/main.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "public_records" {
   for_each = { for record in var.public_records: record.name => record}
-  zone_id  = var.public_zone_id
+  zone_id  = "${var.public_zone_id != "" ? true : false}"
   name     = each.value.name
   type     = each.value.type
   ttl      = each.value.ttl
@@ -9,7 +9,7 @@ resource "aws_route53_record" "public_records" {
 
 resource "aws_route53_record" "private_records" {
   for_each = { for record in var.private_records: record.name => record}
-  zone_id  = var.private_zone_id
+  zone_id  = "${var.private_zone_id != "" ? true : false}"
   name     = each.value.name
   type     = each.value.type
   ttl      = each.value.ttl
--- route53/modules/records/variables.tf
+++ route53/modules/records/variables.tf
@@ -1,8 +1,12 @@
 variable public_zone_id {
+  type        = string
+  default     = ""
   description = "Set public zone id"
 }
 
 variable private_zone_id {
+  type        = string
+  default     = ""
   description = "Set private zone id"
 }
 
