8ad51f560347a1d56db71560ae272dcbbf3cd926
Michael Winchester <mich43l>
2023-03-29 20:04:26 +0700

    update output variable

--- networking/modules/networking/outputs.tf
+++ networking/modules/networking/outputs.tf
@@ -3,9 +3,9 @@ output vpc_id {
 }
 
 output private_subnet_ids {
-  value = var.create_private_subnet ? aws_subnet.private_subnet.*.id : null
+  value = var.create_private_subnet ? aws_subnet.private_subnet.*.id : []
 }
 
 output public_subnet_ids {
-  value = var.create_public_subnet ? aws_subnet.public_subnet.*.id : null
+  value = var.create_public_subnet ? aws_subnet.public_subnet.*.id : []
 }
