430cc382d3aa26c25ef504568a0f51dec321e6ae
mach1el <th3phantom.m1@gmail.com>
2023-03-30 16:38:26 +0700

    update output

--- networking/modules/networking/outputs.tf
+++ networking/modules/networking/outputs.tf
@@ -2,10 +2,18 @@ output vpc_id {
   value = aws_vpc.new_vpc.id
 }
 
+output public_subnet_ids {
+  value = var.create_public_subnet ? aws_subnet.public_subnet.*.id : []
+}
+
 output private_subnet_ids {
   value = var.create_private_subnet ? aws_subnet.private_subnet.*.id : []
 }
 
-output public_subnet_ids {
-  value = var.create_public_subnet ? aws_subnet.public_subnet.*.id : []
+output public_route_table_id {
+  value = var.create_public_subnet ? aws_route_table.PublicRT.*.id : []
+}
+
+output private_route_table_id {
+  value = var.create_private_subnet ? aws_route_table.PrivateRT.*.id : []
 }
