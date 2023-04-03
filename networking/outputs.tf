5570c39bb7ce3661616c53f4911bec0ecc4a618c
mach1el <th3phantom.m1@gmail.com>
2023-04-03 11:23:33 +0700

    add output for igw natgw

--- networking/outputs.tf
+++ networking/outputs.tf
@@ -10,6 +10,14 @@ output private_subnet_ids {
   value = var.create_private_subnet ? aws_subnet.private_subnet.*.id : []
 }
 
+output igw_id {
+  value = aws_internet_gateway.igw.id
+}
+
+output natgw_id {
+  value = aws_nat_gateway.NATgw.id
+}
+
 output public_route_table_id {
   value = var.create_public_subnet ? aws_route_table.PublicRT.*.id : []
 }
