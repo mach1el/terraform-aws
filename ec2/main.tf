f746f01c2bbc21dd2da705700be19c0bcde4ee2e
mach1el <th3phantom.m1@gmail.com>
2023-04-05 14:04:06 +0700

    fix resource var

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,12 +1,12 @@
 resource "aws_instance" "new" {
   for_each = {for server in local.instances: server.instance_name => server}
 
-  ami                             = each.value.ami
-  instance_type                   = each.value.instance_type
-  subnet_id                       = each.value.subnet_id
-  key_name                        = each.value.ssh_key_name
-  vpc_security_group_ids          = each.value.security_groups
-  ecs_associate_public_ip_address = each.value.public_ip
+  ami                         = each.value.ami
+  instance_type               = each.value.instance_type
+  subnet_id                   = each.value.subnet_id
+  key_name                    = each.value.ssh_key_name
+  vpc_security_group_ids      = each.value.security_groups
+  associate_public_ip_address = each.value.public_ip
   
   root_block_device {
     volume_size = each.value.volume_size
