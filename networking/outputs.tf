7b0f1bc28f1459e1c7116beb708215980c770123
mach1el <th3phantom.m1@gmail.com>
2023-04-11 23:50:16 +0700

    update var

--- networking/outputs.tf
+++ networking/outputs.tf
@@ -6,11 +6,11 @@ output vpc_id {
   value = aws_vpc.new_vpc.id
 }
 
-output public_subnets_id {
+output public_subnet_ids {
   value = var.create_public_subnet ? aws_subnet.public_subnet.*.id : []
 }
 
-output private_subnets_id {
+output private_subnet_ids {
   value = var.create_private_subnet ? aws_subnet.private_subnet.*.id : []
 }
 
