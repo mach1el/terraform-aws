bd44188d37436e87a840df301d5b59de0ece5783
mach1el <th3phantom.m1@gmail.com>
2023-04-11 23:47:31 +0700

    rename module

--- eks/modules/iam_roles/outputs.tf
+++
@@ -1,3 +0,0 @@
-output this_cluster {
-  value = aws_iam_role.this_cluster
-}
--- eks/modules/iam_roles/variables.tf
+++
@@ -1,5 +0,0 @@
-variable role_name {
-  type        = string
-  default     = "MyApp-cluster-roles"
-  description = "Naming IAM role for managing EKS resource"
-}
